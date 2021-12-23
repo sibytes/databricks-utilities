25f2124383591d712418dfbd038eeba22d35c27e
shaunryan <shaunryan>
2021-12-23 18:23:55 +0000

    demo

--- mytest/test.py
+++ mytest/test.py
@@ -7,7 +7,5 @@ class Test(unittest.TestCase):
     def test_add(self):
       self.assertEqual(add(2,2), 4)
       
-
-            
 def run_tests():
   unittest.main(argv=[''], verbosity=2, exit=False)
--- utilities/__init__.py
+++ utilities/__init__.py
@@ -2,4 +2,3 @@ from .config import AppConfig
 from .demo import add
 
 __all__ = ["AppConfig", "add"]
-
