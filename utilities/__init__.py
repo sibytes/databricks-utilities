d04586be78e24466f44a892aad7a0872e53c894e
Shaun Ryan <Shaun_Chiburi@hotmail.com>
2021-12-28 16:10:01 +0000

    chor: packaging

--- utilities/__init__.py
+++ utilities/__init__.py
@@ -1,5 +1,6 @@
 from .config import AppConfig
 from .demo import add
 from .logger import get_logger
+from .workflows import Notebook, execute_notebook
 
-__all__ = ["AppConfig", "add", "get_logger"]
+__all__ = ["AppConfig", "add", "get_logger", "Notebook", "execute_notebook"]
