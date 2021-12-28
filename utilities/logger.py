db53746fef148c7757efae36e9e3e6312f11965d
shaunryan <shaunryan>
2021-12-28 22:13:28 +0000

    Azure Logging

--- utilities/logger.py
+++ utilities/logger.py
@@ -1,16 +1,29 @@
-import logging
+import logging, os
+from opencensus.ext.azure.log_exporter import AzureLogHandler
 
-
-def get_logger(name:str):
-
-  console = logging.StreamHandler()
-  console.setLevel(logging.INFO)
-  formatter = logging.Formatter('%(asctime)s %(name)-12s %(levelname)-8s %(message)s', datefmt='%y-%m-%d %H:%M')
-  console.setFormatter(formatter)
-
+def get_logger(name:str, logging_level:int = logging.INFO):
 
   logger = logging.getLogger(name)
-  logger.setLevel(logging.INFO)
-  logger.addHandler(console)
+  logger.setLevel(logging_level)
+  logger.handlers = []
+
+  formatter = logging.Formatter(
+    '%(asctime)s %(name)-12s %(levelname)-8s %(message)s', 
+    datefmt='%y-%m-%d %H:%M')
   
+  console = logging.StreamHandler()
+  console.setLevel(logging_level)
+  console.setFormatter(formatter)
+  
+  logger.addHandler(console)
+
+  # if an app connection insight string is set
+  # then add an azure logger.
+  if os.getenv("APPLICATIONINSIGHTS_CONNECTION_STRING"):
+      azure = AzureLogHandler()
+      azure.setLevel(logging_level)
+      azure.setFormatter(formatter)
+      logger.addHandler(azure)
+      
   return logger
+
