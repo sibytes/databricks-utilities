1aa53ba2d51e830e003f1485a357b4b6fa021545
shaunryan <shaunryan>
2021-12-28 22:31:02 +0000

    log handlers

--- utilities/logger.py
+++ utilities/logger.py
@@ -5,25 +5,27 @@ def get_logger(name:str, logging_level:int = logging.INFO):
 
   logger = logging.getLogger(name)
   logger.setLevel(logging_level)
-  logger.handlers = []
 
   formatter = logging.Formatter(
     '%(asctime)s %(name)-12s %(levelname)-8s %(message)s', 
     datefmt='%y-%m-%d %H:%M')
   
-  console = logging.StreamHandler()
-  console.setLevel(logging_level)
-  console.setFormatter(formatter)
-  
-  logger.addHandler(console)
+  if not any(l.get_name() == "console" for l in logger.handlers):
+    console = logging.StreamHandler()
+    console.setLevel(logging_level)
+    console.setFormatter(formatter)
+    console.set_name("console")
+    logger.addHandler(console)
 
   # if an app connection insight string is set
   # then add an azure logger.
   if os.getenv("APPLICATIONINSIGHTS_CONNECTION_STRING"):
-      azure = AzureLogHandler()
-      azure.setLevel(logging_level)
-      azure.setFormatter(formatter)
-      logger.addHandler(azure)
+      if not any(l.get_name() == "azure_app_insights" for l in logger.handlers):
+          azure = AzureLogHandler()
+          azure.setLevel(logging_level)
+          azure.setFormatter(formatter)
+          azure.set_name("azure_app_insights")
+          logger.addHandler(azure)
       
   return logger
 
